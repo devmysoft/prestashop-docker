9715f5bc8f529b1e9ca24cfc98b5c39f241cee7d
Clément Rosenberger <clement.rosenberger@gmail.com>
2020-04-24 13:47:23 +0200

    add cleardb in docker run

--- base/images/7.2-apache/config_files/docker_run.sh
+++ base/images/7.2-apache/config_files/docker_run.sh
@@ -87,11 +87,11 @@ if [ ! -f ./config/settings.inc.php ] && [ ! -f ./install.lock ]; then
         fi
 
         echo "\n* Launching the installer script..."
-        runuser -g www-data -u www-data -- php -d memory_limit=-1 /var/www/html/$PS_FOLDER_INSTALL/index_cli.php \
+        php -d memory_limit=-1 /var/www/html/$PS_FOLDER_INSTALL/index_cli.php \
         --domain="$PS_DOMAIN" --db_server=$DB_SERVER:$DB_PORT --db_name="$DB_NAME" --db_user=$DB_USER \
         --db_password=$DB_PASSWD --prefix="$DB_PREFIX" --firstname="John" --lastname="Doe" \
         --password=$ADMIN_PASSWD --email="$ADMIN_MAIL" --language=$PS_LANGUAGE --country=$PS_COUNTRY \
-        --all_languages=$PS_ALL_LANGUAGES --newsletter=0 --send_email=0 --ssl=$PS_ENABLE_SSL
+        --all_languages=$PS_ALL_LANGUAGES --newsletter=0 --send_email=0 --ssl=$PS_ENABLE_SSL --db_clear=$PS_DB_CLEAR
 
         if [ $? -ne 0 ]; then
             echo 'warning: PrestaShop installation failed.'
